cb0c953e82d41215f21e6ee7a6a55e2c00ae0b7e
Yanis Bennadij <yanis.bennadji@laplateforme.io>
2024-05-13 16:30:09 +0200

    usdhfdsjk
    qsd

---
+++ code/html/marseille.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <section>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, pariatur illum vel sed iste non ex ab quo cupiditate rerum odio! Ullam fugit repellendus tempore dolorum sequi pariatur cumque dignissimos?</p>
+    </section>
+    
+</body>
+</html>
