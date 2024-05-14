ed488c3dec52434fd0733b51a97625e1c3900ed4
Maiilaa <zirarimaila8@gmail.com>
2024-05-14 11:46:32 +0200

    1

---
+++ code/html/malaisie.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Malaisie</title>
+    <link rel="stylesheet" href="malaisie.css">
+</head>
+<body>
+    <header>
+ 
+    </header>
+    <main>
+    <h1>MALAISIE</h1>
+    <img class= "plagemalaisie"src="../../assets/image/maïla/plagemalaisie.jpg" alt="plagemalaisie">
+    </main>
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
