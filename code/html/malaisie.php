24d6cdbecfef871a6c89b8961471368995c1b6b5
Maiilaa <zirarimaila8@gmail.com>
2024-05-14 13:11:56 +0200

    2

--- code/html/malaisie.php
+++ code/html/malaisie.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Malaisie</title>
-    <link rel="stylesheet" href="malaisie.css">
+    <link rel="stylesheet" href="../css/malaisie.css">
 </head>
 <body>
     <header>
