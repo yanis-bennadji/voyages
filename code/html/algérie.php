bbbdb953a541221af32cc2f97e555dc50f7e4be7
bailhamza <h25667298@gmail.com>
2024-05-14 21:16:13 +0200

    mise a jour

--- code/html/algérie.php
+++ code/html/algérie.php
@@ -15,10 +15,10 @@
    
       <section class="smenu">
        <menu class="algmenu" >
-         <h4  id="histoire">Histoire</h4>
-         <h4 id="geographie">Géographie</h4>
-         <h4 id="capital">Capitale</h4>
-         <h4 id="lieu">Lieux à visiter</h4>
+         <h4 > Histoire</h4>
+         <a href="#geo"><h4>Géographie</h4></a>
+         <a href="#capitale"><h4>Capitale</h4></a>
+          <a href="#lieux"><h4 >Lieux à visiter</h4></a>
         </menu>
         </section>
 <section class="carpar">
@@ -34,33 +34,24 @@
             <img class="imgcaroussel1" src="../../assets/image/hamza/algb.jpg" alt="">
         </div>
     </section>
-    <section class="par">
+  <section class="par">
     <p>L'Algérie , arabe : الجزائر (al-Jazā'ir)Note 2,Note 3 ; et arabe <br> algérien : دزاير (Dzayer) ou الجزاير (El Djazaïr) ; en tamazight ⴷⵣⴰⵢⵔ (Dzayer)) <br> est un pays d’Afrique du Nord faisant partie du Maghreb. Depuis 1962, <br> elle est nommée en forme longue République algérienne démocratique et populaire, <br> abrégée en (RADP) (en arabe : الجمهورية الجزائرية الديمقراطية الشعبية ; en tamazight <br>: ⵜⴰⴳⴷⵓⴷⴰ ⵜⴰⵎⴳⴷⴰⵢⵜ ⵜⴰⵖⵔⴼⴰⵏⵜ ⵜⴰⴷⵣⴰⵢⵔⵉⵜ (Tagduda tamegdayt taɣerfant tazzayrit))10. Sa capitale est <br>Alger, la ville la plus peuplée du pays, dans le Nord, sur la côte méditerranéenne.</p>
     <p>Au 1er janvier 2021, la population algérienne résidente avait atteint 44,6 millions <br> d’habitants11. Avec une superficie de 2 381 741 km2, c'est le <br> dixième plus grand pays au monde et le plus grand pays d'Afrique, <br> du monde arabe et du bassin méditerranéen. Elle partage plus de 6 343 km de frontières <br> terrestres, avec la Tunisie au nord-est, la Libye à l'est, le Niger <br> au sud-est, le Mali au sud-ouest, la Mauritanie et <br> le Sahara occidental à l'ouest, et enfin le Maroc au nord-ouest12.</p>
+  </section>
+</section>
 
-    </section>
+  <h2 id="geo"> Géographie</h2>
+<section class="sgeo">
+<p class="pgeo">L'Algérie est un pays constitué d'une multitude de reliefs. Le nord est sillonné d'ouest en est <br> par une double barrière montagneuse (Atlas tellien et saharien) avec des chaînes telles que <br> le Dahra, l'Ouarsenis, le Hodna, les  chaines de Kabylie (le Djurdjura, les Babors et <br> les Bibans) et l'Aurès. Le sol est couvert de nombreuses forêts au centre, <br> vastes plaines à l'est et le Sahara qui représente à lui <br> seul 84 % du territoire. La superficie du pays est la plus grande d'Afrique, avec 2 381 741 km2 .</p>
+<p class="pgeo">Avec une superficie de 2 381 741 km2, c'est le dixième plus grand pays au monde et le plus grand pays d'Afrique du monde arabe et du bassin méditerranéen. Elle partage plus de 6 343 km de frontières terrestres, avec la Tunisie au nord-est, la Libye à l'est, le Niger au sud-est, le Mali au sud-ouest, la Mauritanie et le Sahara occidental à l'ouest, et enfin le Maroc au nord-ouest12.</p>
+</section>
+<h2 id="capitale">Capitale</h2>
+<section class="salger">
+  <p class="palger">Alger (en arabe : الجزائر العاصمة, Al-Jazāʾir El ʿĀṣimanote 1,2, en berbère : ⵍⴷⵣⴰⵢⵔ ⵜⴰⵎⴰⵏⴰⵖⵜ, Ldzayer tamanaɣt ou Lezzayer tamanaɣt3), surnommée El Bahdja (« la joyeuse »), El Mahrussa (« la bien-gardée ») ou El Beida (« la blanche »), est la capitale de l'Algérie et en est la ville la plus peuplée.</p>
+  <p class="palger">Située au bord de la mer Méditerranée, la ville d'Alger est en fait constituée de plusieurs communes de la wilaya d'Alger dont elle donne son nom en tant que chef-lieu mais n'a ni personnalité juridique, ni structure d'administration en propre. L'unité urbaine d'Alger comptait 2 481 788 habitants selon l'Office national des statistiques algérien d'après le dernier recensement de 20084.</p>
+  <p class="palger">Fondée au ive siècle av. J.-C. comme comptoir par les phéniciens, sous le nom d'Icosium, elle est occupée par les Romains, les Vandales, les Byzantins et les Arabes, puis au début du Moyen Âge par la tribu berbère des Beni-Mezghana, avant d'être récupérée en 1204 par la tribu arabe des Thaâliba qui règneront sur la région d'Alger jusqu'en 15168.</p>
 </section>
    
-
-        
-
-
-
-
-       
-        
-
 </main>
-  
-
-  
-
-        
-   
-    
-    
-    
-    
-       
-</body>
+  </body>
 </html>
