e098b27681b081eef88a1cb4ee6252236128644e
Maiilaa <zirarimaila8@gmail.com>
2024-05-14 16:01:01 +0200

    border titre

--- code/html/malaisie.php
+++ code/html/malaisie.php
@@ -14,10 +14,11 @@
     <h1>MALAISIE</h1>
     <img class= "images"src="../../assets/image/maïla/kualalampur.jpeg" alt="kualalampur">
     <h2>Kuala Lampur, capitale historique</h2>
+    <p>La capitale de la Malaisie saura vous intriguer avec ses tours jumelles. Il est impossible que ces deux édifices vous laissent indifférent : symboles de Kuala Lumpur, elles s’élèvent jusqu’à 452 mètres de hauteur et disposent de 88 étages. Les Tours Petronas regroupent en leur sein des bureaux, des boutiques, encore un centre multimédia. Les deux tours sont reliées par ce que l’on appelle un skybridge qui se situe à 170 mètres au-dessus du sol. Pour avoir une vue imprenable sur la ville et ses environs, pour comprendre toute la majesté de ce lieu, les Tours Petronas sont absolument à faire à Kuala Lumpur.</p>
     <img class= "images"src="../../assets/image/maïla/plagemalaisie.jpg" alt="plagemalaisie">
     <h2>Plage de Langkawi</h2>
     <img class= "images"src="../../assets/image/maïla/MontKinabalu.jpeg" alt="montkinabalu">
-    <h3>Mont Kinabalu</h3>
+    <h2>Mont Kinabalu</h2>
     <p></p>
     </main>
     <footer>
