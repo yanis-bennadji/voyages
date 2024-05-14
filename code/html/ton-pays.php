1436d9e04c1eab598a6c25fb3559bf5f998cd5bf
Guiiizmo <malik.aboudou@laplateforme.io>
2024-05-14 16:46:51 +0200

    blabla

--- code/html/ton-pays.php
+++ code/html/ton-pays.php
@@ -18,18 +18,22 @@
             <p>
                 En France, il n'y a pas trop de coutumes ou de traditions particulières mais il y a beaucoup de fêtes religieuse ou pour les jours de gloire du Pays. Nous avons par exemple la fête nationale du 14 juillet qui représente la date significative de la prise de la Bastille, signe de la fin de la monarchie absolue, nous avons aussi le jour de pâques qui représente la rennaissance du Christ.
             </p>
-            <img class="istp" src= "../../assets/image/malik/pdb.jpg" alt= "Prise da la bastille" />
-            <img class="istp" src= "../../assets/image/malik/paques.jpg" alt= "oeufs de paques" />
+            <div class="imgbox">
+               <img class="istp" src= "../../assets/image/malik/pdb.jpg" alt= "Prise da la bastille" />
+               <img class="istp" src= "../../assets/image/malik/paques.jpg" alt= "oeufs de paques" />
+            </div>
         </div>  
         <div class="sbox">
             <h2>
                 Culture
             </h2>      
             <p>
-                La culture française est principalement axé sur la mode et la gastronomie. Paris est notamment connue pour sa grande avenue des Champs-Elysé, une avenue ou il y a beaucoup de boutique vestimentaires de luxe. Du côte culinaire nous avons tout d'abord la baguette, ensuite les flans et plein d'autre patisserie.
+                La culture française est principalement axé sur la mode et la gastronomie. Paris est notamment connue pour sa grande avenue des Champs-Elysées, une avenue ou il y a beaucoup de boutique vestimentaires de luxe. Du côte culinaire nous avons tout d'abord la baguette, ensuite les flans et plein d'autre patisserie.
             </p>
-            <img class="istp" src= "../../assets/image/malik/kounde.jpg" alt= "Kounde devant la tour eiffel" />
-            <img class="istp" src= "../../assets/image/malik/patisseries.jpg" alt= "patisseries françaises" />
+            <div class="imgbox">
+               <img class="istp" src= "../../assets/image/malik/kounde.jpg" alt= "Kounde devant la tour eiffel" />
+               <img class="istp" src= "../../assets/image/malik/patisseries.jpg" alt= "patisseries françaises" />
+            </div>
         </div>
         <div class="pbox">
             <h2>
@@ -38,9 +42,36 @@
             <p>
                 La France occupe une place de premier plan dans le monde du sport, tant au niveau national qu'international. À l'échelle nationale, le sport fait partie intégrante de la culture française, renforçant l'identité nationale et la fierté. Le pays accueille également des événements sportifs de renommée mondiale, comme le Tour de France et Roland-Garros. Sur la scène internationale, la France est un acteur clé dans les instances dirigeantes du sport et a accueilli plusieurs grands événements sportifs. Enfin, les Français excellent dans divers domaines sportifs, produisant des performances remarquables. Par exemple, des athlètes français ont remporté des médailles d'or aux Jeux olympiques dans des disciplines telles que la natation, l'escrime, et le judo. De plus, des équipes françaises comme l'équipe de football ont remporté des titres majeurs, tels que la Coupe du Monde FIFA, démontrant ainsi la compétitivité et le talent exceptionnels des sportifs français à l'échelle mondiale. 
             </p>
-            <img class="istp" src= "../../assets/image/malik/cdm.jpg" alt="Léequie de france soulève le trophée de la coupe du monde"/>
-            <img class="istp" src="../../assets/image/malik/mo.jpg" alt="francais avec une médaille d'or des jo"/>
+            <div class="imgbox">
+               <img class="istp" src= "../../assets/image/malik/cdm.jpg" alt="Léequie de france soulève le trophée de la coupe du monde"/>
+               <img class="istp" src="../../assets/image/malik/mo.jpg" alt="francais avec une médaille d'or des jo"/>
+            </div>
         </div>
     </div>
+    <h3>
+        Hymne nationale de la France
+    </h3>
+    <iframe src="../../assets/image/malik/marseillaise.mp4" title="Hymne Francaise" width="1320" height="300"></iframe>
+    <h3>
+        Les monuments
+    </h3>
+    <div class="gbox">
+        <div class="boxinvisible">
+          <img class="eiffel" src="../../assets/image/malik/eiffel.jpg" alt="Tour eifeeil qui brille">
+          <p>
+            La Tour Eiffel, emblème emblématique de Paris et de la France, est l'une des structures les plus célèbres au monde, conçue par l'ingénieur Gustave Eiffel et inaugurée en 1889 lors de l'Exposition universelle.
+          </p>
+        </div>
+        <div class="boxinvisible">
+          <img class="adt" src="../../assets/image/malik/adt.jpg" alt="Arc de triomphe qui brille">
+          <p>
+            L'Arc de Triomphe est un monument emblématique situé au cœur de Paris, inaugurée en 1806. Commandé par Napoléon Bonaparte, il célèbre les victoires militaires françaises et honore les soldats tombés au combat. Conçu par l'architecte Jean-François Chalgrin, il est orné de sculptures et d'inscriptions commémoratives. Au sommet de l'arc, une terrasse offre une vue panoramique spectaculaire de la ville. Symbole de la grandeur et de la résilience nationale, l'Arc de Triomphe est un incontournable pour les visiteurs de Paris.
+          </p>
+        </div> 
+        <div class="boxinvisible">
+          <img class="versailles" src="../../assets/image/malik/versailles.jpg" alt="chateau de versailles">
+          <p>
+            Le Château de Versailles, résidence royale emblématique située près de Paris, est un chef-d'œuvre de l'architecture française. Construit au XVIIe siècle sous la direction de Louis XIV, il est renommé pour sa splendeur et son opulence.
+          </p>  
 </body>
 </html>
