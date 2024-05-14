301c96f9507517906e833f97c85940bf92fd510c
hsabrina1 <hadi.sabrina@hotmail.com>
2024-05-14 13:53:38 +0200

    partie2

--- code/html/ilemaurice.php
+++ code/html/ilemaurice.php
@@ -4,16 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/ilemaurice.css">
 </head>
 <body>
 
-    ILE MAURICE
+    <div class="mauricehaut">
+    
     <img src= "../../assets/image/sabrina/logo-ile-maurice1.png">
+    </div>
+
     <img src= "../../assets/image/sabrina/ile-maurice-1.jpg">
     <p> Située dans l’archipel des Mascareignes au cœur de l’océan Indien,
          l’île Maurice se distingue par sa superbe barrière de corail et ses belles plages de sable fin.
          La capitale Port-Louis, qui est aussi la plus grande ville de l'île Maurice, est située dans le sud-ouest de l'île.
     </p>
+
+    <img src="../../assets/image/sabrina/cartemaurice.jpg">
+    <img src="../../assets/image/sabrina/caselanatureparks.jpg">
 
 </body>
 </html>
