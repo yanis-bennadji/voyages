a848d6d21ec62527e1b478cecbbed15c12236109
Dounayha <dounayha05@gmail.com>
2024-05-14 13:41:59 +0200

    html fait

---
+++ code/html/maldives.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maldives</title>
+    <link rel="stylesheet" href="../css/maldives.css">
+</head>
+<body>
+<main>
+      <img src="../../assets/image/dounayha/maldivesPlage.jpg" alt="">
+       <h1>
+        Les maldives
+        </h1>
+
+        <div class="descriptionMaldives">
+         <p>
+         Baigné par les eaux étincelantes de l'océan Indien, l'archipel des Maldives est l'une des destinations les plus prisées au monde. Les eaux des Maldives, terrain de jeu naturel de nombreuses espèces, parmi lesquelles 21 espèces de dauphins et de baleines, abritent une vie sous-marine dynamique.
+         </p>
+        </div>
+    
+        <h2>
+         UN HAVRE DE PAIX INSULAIRE PRIVÉ
+        </h2>
+
+        <div class="descriptionH2">
+         <p>
+         Le One&Only Reethi Rah est situé sur l'une des plus grandes îles de l'atoll de Malé Nord. Il est niché au cœur d'un archipel baigné par les eaux étincelantes abritant des milliers d'îles coralliennes et berceau d'une vie marine dynamique. Explorez les mystères que cache l'azur de ce terrain de jeu aquatique naturel et partez à la rencontre des espèces les plus fascinantes. Profitez du romantisme qui se dégage de cette île célèbre.
+         </p>
+         <img src="../../assets/image/dounayha/maldives2.jpg" alt="">
+        </div>
+
+           <section class="DescriptionVillaPlage">
+              <h2>
+                VILLAS DE PLAGE
+              </h2>
+              <img src="../../assets/image/dounayha/villaPlage.jpg" alt="">
+              <p>
+                 Dans nos élégantes villas de plage s'ouvrant sur l'océan, faites l'expérience de la vie insulaire dans sa plus grande pureté.
+              </p>
+           </section>
+
+             <section class="DescriptionVillaPilotis">
+             <h2>
+             VILLAS SUR PILOTIS
+             </h2>
+             <img src="../../assets/image/dounayha/villaPilotis.webp" alt="">
+             <p>
+             Profitez de la mer dans nos incroyables villas sur pilotis pour un séjour tropical inoubliable aux Maldives.
+             </p>
+             </section>
+           
+          
+
+    </main>
+</body>
+</html>
