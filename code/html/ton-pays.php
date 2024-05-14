a3f9de9e801418033e8e982f235f1ffb899cded0
Guiiizmo <malik.aboudou@laplateforme.io>
2024-05-14 14:19:39 +0200

    Première version test

---
+++ code/html/ton-pays.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="../css/france.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>France</title>
+</head>
+<body>
+    <h1>
+        Présentation de la France
+    </h1>
+    <div class="gbox">
+        <div class="cbox">
+            <h2>
+                Les coutumes
+            </h2>
+            <p>
+                En France, il n'y a pas trop de coutumes ou de traditions particulières mais il y a beaucoup de fêtes religieuse ou pour les jours de gloire du Pays. Nous avons par exemple la fête nationale du 14 juillet qui représente la date significative de la prise de la Bastille, signe de la fin de la monarchie absolue, nous avons aussi le jour de pâques qui représente la rennaissance du Christ.
+            </p>
+            <img class="istp" src= "../../assets/image/malik/pdb.jpg" alt= "Prise da la bastille" />
+            <img class="istp" src= "../../assets/image/malik/paques.jpg" alt= "oeufs de paques" />
+        </div>  
+        <div class="sbox">
+            <h2>
+                Culture
+            </h2>      
+            <p>
+                La culture française est principalement axé sur la mode et la gastronomie. Paris est notamment connue pour sa grande avenue des Champs-Elysé, une avenue ou il y a beaucoup de boutique vestimentaires de luxe. Du côte culinaire nous avons tout d'abord la baguette, ensuite les flans et plein d'autre patisserie.
+            </p>
+            <img class="istp" src= "../../assets/image/malik/kounde.jpg" alt= "Kounde devant la tour eiffel" />
+            <img class="istp" src= "../../assets/image/malik/patisseries.jpg" alt= "patisseries françaises" />
+        </div>
+        <div class="pbox">
+            <h2>
+                Sport
+            </h2>
+            <p>
+                La France occupe une place de premier plan dans le monde du sport, tant au niveau national qu'international. À l'échelle nationale, le sport fait partie intégrante de la culture française, renforçant l'identité nationale et la fierté. Le pays accueille également des événements sportifs de renommée mondiale, comme le Tour de France et Roland-Garros. Sur la scène internationale, la France est un acteur clé dans les instances dirigeantes du sport et a accueilli plusieurs grands événements sportifs. Enfin, les Français excellent dans divers domaines sportifs, produisant des performances remarquables. Par exemple, des athlètes français ont remporté des médailles d'or aux Jeux olympiques dans des disciplines telles que la natation, l'escrime, et le judo. De plus, des équipes françaises comme l'équipe de football ont remporté des titres majeurs, tels que la Coupe du Monde FIFA, démontrant ainsi la compétitivité et le talent exceptionnels des sportifs français à l'échelle mondiale. 
+            </p>
+            <img class="istp" src= "../../assets/image/malik/cdm.jpg" alt="Léequie de france soulève le trophée de la coupe du monde"/>
+            <img class="istp" src="../../assets/image/malik/mo.jpg" alt="francais avec une médaille d'or des jo"/>
+        </div>
+    </div>
+</body>
+</html>
