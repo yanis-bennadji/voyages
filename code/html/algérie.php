0280215fff3a93e074a64e555deb1930d23accab
bailhamza <h25667298@gmail.com>
2024-05-14 16:55:06 +0200

    avancé de la page

--- code/html/algérie.php
+++ code/html/algérie.php
@@ -3,22 +3,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Algérie</title>
+    <link rel="stylesheet" href="../css/algérie.css">
 </head>
 <body>
-    <main>
-    <h1>l'Algerie</h1>
-    <menu>
-        <h4 id="histoire">Histoire</h4>
-        <h4 id="geographie">Géographie</h4>
-        <h4 id="capital">Capital</h4>
-        <h4 id="lieu">Lieu à visiter</h4>
+  <main>
+    <h1 class="algt">l'Algerie</h1>
+    <section class="simg">
+    <img class="imgdrp" src="../../assets/image/hamza/drapeaualg.jpg" alt="">
+    <img class="imgcarte" src="../../assets/image/hamza/catrealg.png" alt="">
+    </section>
+   
+      <section class="smenu">
+       <menu class="algmenu" >
+         <h4  id="histoire">Histoire</h4>
+         <h4 id="geographie">Géographie</h4>
+         <h4 id="capital">Capitale</h4>
+         <h4 id="lieu">Lieux à visiter</h4>
+        </menu>
+        </section>
+<section class="carpar">
+   <section class="scaroussel1">
+        <div class="caroussel1">
+            <img class="imgcaroussel1" src="../../assets/image/hamza/algc.jpg" alt="">
+            <img class="imgcaroussel1" src="../../assets/image/hamza/alga.jpg" alt="">
+            <img class="imgcaroussel1" src="../../assets/image/hamza/algd.jpg" alt="">
+            <img class="imgcaroussel1" src="../../assets/image/hamza/alge.jpg" alt="">
+            <img class="imgcaroussel1" src="../../assets/image/hamza/algf.jpg" alt="">
+            <img class="imgcaroussel1" src="../../assets/image/hamza/algg.jpg" alt="">
+            <img class="imgcaroussel1" src="../../assets/image/hamza/algh.jpg" alt="">
+            <img class="imgcaroussel1" src="../../assets/image/hamza/algb.jpg" alt="">
+        </div>
+    </section>
+    <section class="par">
+    <p>L'Algérie , arabe : الجزائر (al-Jazā'ir)Note 2,Note 3 ; et arabe <br> algérien : دزاير (Dzayer) ou الجزاير (El Djazaïr) ; en tamazight ⴷⵣⴰⵢⵔ (Dzayer)) <br> est un pays d’Afrique du Nord faisant partie du Maghreb. Depuis 1962, <br> elle est nommée en forme longue République algérienne démocratique et populaire, <br> abrégée en (RADP) (en arabe : الجمهورية الجزائرية الديمقراطية الشعبية ; en tamazight <br>: ⵜⴰⴳⴷⵓⴷⴰ ⵜⴰⵎⴳⴷⴰⵢⵜ ⵜⴰⵖⵔⴼⴰⵏⵜ ⵜⴰⴷⵣⴰⵢⵔⵉⵜ (Tagduda tamegdayt taɣerfant tazzayrit))10. Sa capitale est <br>Alger, la ville la plus peuplée du pays, dans le Nord, sur la côte méditerranéenne.</p>
+    <p>Au 1er janvier 2021, la population algérienne résidente avait atteint 44,6 millions <br> d’habitants11. Avec une superficie de 2 381 741 km2, c'est le <br> dixième plus grand pays au monde et le plus grand pays d'Afrique, <br> du monde arabe et du bassin méditerranéen. Elle partage plus de 6 343 km de frontières <br> terrestres, avec la Tunisie au nord-est, la Libye à l'est, le Niger <br> au sud-est, le Mali au sud-ouest, la Mauritanie et <br> le Sahara occidental à l'ouest, et enfin le Maroc au nord-ouest12.</p>
+
+    </section>
+</section>
+   
 
         
-    </menu>
-    <p>L'Algérie , arabe : الجزائر (al-Jazā'ir)Note 2,Note 3 ; et arabe <br> algérien : دزاير (Dzayer) ou الجزاير (El Djazaïr) ; en tamazight ⴷⵣⴰⵢⵔ (Dzayer)) <br> est un pays d’Afrique du Nord faisant partie du Maghreb. Depuis 1962, <br> elle est nommée en forme longue République algérienne démocratique et populaire, abrégée en (RADP) (en arabe : الجمهورية الجزائرية الديمقراطية الشعبية ; en tamazight : ⵜⴰⴳⴷⵓⴷⴰ ⵜⴰⵎⴳⴷⴰⵢⵜ ⵜⴰⵖⵔⴼⴰⵏⵜ ⵜⴰⴷⵣⴰⵢⵔⵉⵜ (Tagduda tamegdayt taɣerfant tazzayrit))10. Sa capitale est <br>Alger, la ville la plus peuplée du pays, dans le Nord, sur la côte méditerranéenne.</p>
-    <p>Au 1er janvier 2021, la population algérienne résidente avait atteint 44,6 millions <br> d’habitants11. Avec une superficie de 2 381 741 km2, c'est le <br> dixième plus grand pays au monde et le plus grand pays d'Afrique, <br> du monde arabe et du bassin méditerranéen. Elle partage plus de 6 343 km de frontières <br> terrestres, avec la Tunisie au nord-est, la Libye à l'est, le Niger <br> au sud-est, le Mali au sud-ouest, la Mauritanie et <br> le Sahara occidental à l'ouest, et enfin le Maroc au nord-ouest12.</p>
-    j
-    </main>
+
+
+
+
+       
+        
+
+</main>
+  
+
+  
+
+        
+   
+    
+    
     
     
        
