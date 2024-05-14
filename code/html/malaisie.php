4bb2daddf0a3ae47991ff582f7cdd420c4ece9f8
Maiilaa <zirarimaila8@gmail.com>
2024-05-14 16:59:40 +0200

    fn de journée

--- code/html/malaisie.php
+++ code/html/malaisie.php
@@ -14,9 +14,10 @@
     <h1>MALAISIE</h1>
     <img class= "images"src="../../assets/image/maïla/kualalampur.jpeg" alt="kualalampur">
     <h2>Kuala Lampur, capitale historique</h2>
-    <p>La capitale de la Malaisie saura vous intriguer avec ses tours jumelles. Il est impossible que ces deux édifices vous laissent indifférent : symboles de Kuala Lumpur, elles s’élèvent jusqu’à 452 mètres de hauteur et disposent de 88 étages. Les Tours Petronas regroupent en leur sein des bureaux, des boutiques, encore un centre multimédia. Les deux tours sont reliées par ce que l’on appelle un skybridge qui se situe à 170 mètres au-dessus du sol. Pour avoir une vue imprenable sur la ville et ses environs, pour comprendre toute la majesté de ce lieu, les Tours Petronas sont absolument à faire à Kuala Lumpur.</p>
+    <p>La capitale de la Malaisie saura vous intriguer avec ses tours jumelles. Il est impossible que ces deux édifices vous laissent indifférent: symboles de Kuala Lumpur, elles s’élèvent jusqu’à 452 mètres de hauteur et disposent de 88 étages. Les Tours Petronas regroupent en leur sein des bureaux, des boutiques,un centre multimédia. Les deux tours sont reliées par un skybridge qui se situe à 170 mètres au-dessus du sol. Pour avoir une vue imprenable sur la ville et ses environs, pour comprendre toute la majesté de ce lieu, les Tours Petronas sont absolument à faire à Kuala Lumpur.</p>
     <img class= "images"src="../../assets/image/maïla/plagemalaisie.jpg" alt="plagemalaisie">
     <h2>Plage de Langkawi</h2>
+    <p>Au nord-ouest de la Malaisie, à la limite de la frontière thaï, s'étend un archipel de 99 îles féeriques, les îles Langkawi.Les îles Langkawi offrent des paysages resplendissants, sauvages et fantasmatiques : de hautes falaises surmontées de forêts vierges plongent dans la mer, découpant criques, baies et grottes inaccessibles. Aujourd'hui, Langkawi, l'île principale, est un pôle touristique majeur, regroupant complexes hôteliers et gros investisseurs. L’île n'a pas encore totalement perdu son caractère authentique, mais ça ne saurait tarder.</p>
     <img class= "images"src="../../assets/image/maïla/MontKinabalu.jpeg" alt="montkinabalu">
     <h2>Mont Kinabalu</h2>
     <p></p>
