7c8b650b508dda69b409cde9983890c941fbac7b
Maiilaa <zirarimaila8@gmail.com>
2024-05-15 16:55:37 +0200

    16h55

--- code/html/malaisie.php
+++ code/html/malaisie.php
@@ -16,17 +16,17 @@
                 <section>
                     <img class= "images"src="../../assets/image/maïla/kualalampur.jpeg" alt="kualalampur">
                     <h2>Kuala Lampur, capitale historique</h2>
-                    <p>La capitale de la Malaisie saura vous intriguer avec ses tours jumelles.Symbole de Kuala Lumpur, elles s’élèvent jusqu’à 452 mètres de hauteur et disposent de 88 étages. Les Tours Petronas regroupent en leur sein des bureaux, des boutiques,un centre multimédia. Les deux tours sont reliées par un skybridge qui se situe à 170 mètres au-dessus du sol. Pour avoir une vue imprenable sur la ville et ses environs, pour comprendre toute la majesté de ce lieu, les Tours Petronas sont absolument à faire à Kuala Lumpur.</p>
+                    <p>La capitale de la Malaisie saura vous intriguer avec ses tours jumelles. Les Tours Petronas regroupent en leur sein des bureaux, des boutiques,un centre multimédia.Elles sont reliées par un skybridge qui se situe à 170 mètres de hauteur. Pour avoir une vue imprenable sur la ville et ses environs, les Tours Petronas sont absolument à faire à Kuala Lumpur.</p>
                 </section>
                 <section>
                     <img class= "images"src="../../assets/image/maïla/plagemalaisie.jpg" alt="plagemalaisie">
                     <h2>Plage de Langkawi</h2>
-                    <p>Au nord-ouest de la Malaisie, à la limite de la frontière thaï, s'étend un archipel de 99 îles, les îles Langkawi.Les îles Langkawi offrent des paysages resplendissants: de hautes falaises surmontées de forêts vierges plongent dans la mer. Aujourd'hui, Langkawi, l'île principale, est un pôle touristique majeur, regroupant hôtels et restauration.</p>
+                    <p>Au nord-ouest de la Malaisie, s'étend un archipel de 99 îles dont les îles Langkawi.Les îles Langkawi offrent des paysages resplendissants,de hautes falaises surmontées de forêts vierges plongent dans la mer. Aujourd'hui, Langkawi est un pôle touristique majeur, regroupant hôtels et restauration.</p>
                 </section>
                 <section>
                     <img class= "images"src="../../assets/image/maïla/MontKinabalu.jpeg" alt="montkinabalu">
                     <h2>Mont Kinabalu</h2>
-                    <p>Le Mont Kinabalu est un site touristique incontournable situé à Kota Belud, en Malaisie. Cette montagne majestueuse est la plus haute de tout le pays, offrant des vues à couper le souffle depuis son sommet. L'ascension du Mont Kinabalu est une expérience unique en son genre, permettant aux visiteurs de se rapprocher de la nature et de profiter de paysages spectaculaires. Les amateurs de randonnée apprécieront également les sentiers pittoresques et les cascades environnantes.</p>
+                    <p>Le Mont Kinabalu est un site touristique incontournable situé à Kota Belud, en Malaisie. Cette montagne majestueuse est la plus haute de tout le pays, offrant des vues à couper le souffle depuis son sommet. L'ascension du Mont Kinabalu est une expérience unique en son genre, permettant aux visiteurs de profiter de paysages spectaculaires. Les amateurs de randonnée apprécieront également les cascades environnantes.</p>
                 </section>
             </figure>
     </main>
