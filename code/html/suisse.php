640da6d6a9eba4290c03a591809cd05407e75d99
Ibrahimpalisse <ibrahim.vignes@laplateforme.io>
2024-05-15 10:09:30 +0200

    commit1
    10% de ma page

---
+++ code/html/suisse.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/suisse.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">
+    <title>La suisse</title>
+</head>
+<body>
+
+<header>
+
+</header>
+
+    <main>
+      <section class="container1">
+          <h1>LA SUISSE</h1>
+          <img class="imagemobile1" src="../../assets/image/ibrahim/suiss1.PNG" ait="">
+     </section>
+      
+            
+     <section class="container2">
+       <p>La suisse</p>
+       <p>géografie</p>
+       <p>lieux à vister</p>
+       <p>culinaires suisse</p>
+     </section>
+     <section>
+       <img src="suisse3.jpg" alt="">
+        
+     </section>
+    </main>
+<footer>
+
+</footer>
+    
+</body>
+</html>
