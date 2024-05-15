9f193648cb56bac7af02d64f10c5d4a28f523063
Maiilaa <zirarimaila8@gmail.com>
2024-05-15 09:51:51 +0200

    9h

--- code/html/malaisie.php
+++ code/html/malaisie.php
@@ -20,7 +20,7 @@
     <p>Au nord-ouest de la Malaisie, à la limite de la frontière thaï, s'étend un archipel de 99 îles féeriques, les îles Langkawi.Les îles Langkawi offrent des paysages resplendissants, sauvages et fantasmatiques : de hautes falaises surmontées de forêts vierges plongent dans la mer, découpant criques, baies et grottes inaccessibles. Aujourd'hui, Langkawi, l'île principale, est un pôle touristique majeur, regroupant complexes hôteliers et gros investisseurs. L’île n'a pas encore totalement perdu son caractère authentique, mais ça ne saurait tarder.</p>
     <img class= "images"src="../../assets/image/maïla/MontKinabalu.jpeg" alt="montkinabalu">
     <h2>Mont Kinabalu</h2>
-    <p></p>
+    <p>Le Mont Kinabalu est un site touristique incontournable situé à Kota Belud, en Malaisie. Cette montagne majestueuse est la plus haute de tout le pays, offrant des vues à couper le souffle depuis son sommet. L'ascension du Mont Kinabalu est une expérience unique en son genre, permettant aux visiteurs de se rapprocher de la nature et de profiter de paysages spectaculaires. De plus, la région environnante regorge d'autres attractions intéressantes, telles que le parc national de Kinabalu, où l'on peut découvrir une faune et une flore exceptionnelles. Les amateurs de randonnée apprécieront également les sentiers pittoresques et les cascades environnantes.</p>
     </main>
     <footer>
 
