6630a469150376acbba695f72b8e095b7d198240
Dounayha <dounayha05@gmail.com>
2024-05-15 11:51:05 +0200

    taille image villa

--- code/html/maldives.php
+++ code/html/maldives.php
@@ -9,29 +9,32 @@
 <body>
 <main>
       
-      <img class="maldivesplage" src="../../assets/image/dounayha/maldivesPlage.jpg" alt="">
-       <h1>
-        Les maldives
-        </h1>
+      <img class="maldivesplage" src="../../assets/image/dounayha/maldivesPlage.jpg" alt="">  
+      
 
         <div class="descriptionMaldives">
+          <h1>
+          Les maldives
+          </h1>
+
          <p>
          Baigné par les eaux étincelantes de l'océan Indien, l'archipel des Maldives est l'une des destinations les plus prisées au monde. Les eaux des Maldives, terrain de jeu naturel de nombreuses espèces, parmi lesquelles 21 espèces de dauphins et de baleines, abritent une vie sous-marine dynamique.
          </p>
         </div>
-    
-        <h2>
-         UN HAVRE DE PAIX INSULAIRE PRIVÉ
-        </h2>
 
         <div class="descriptionH2">
+          <h2>
+         UN HAVRE DE PAIX INSULAIRE PRIVÉ
+         </h2>
          <p>
          Le One&Only Reethi Rah est situé sur l'une des plus grandes îles de l'atoll de Malé Nord. Il est niché au cœur d'un archipel baigné par les eaux étincelantes abritant des milliers d'îles coralliennes et berceau d'une vie marine dynamique. Explorez les mystères que cache l'azur de ce terrain de jeu aquatique naturel et partez à la rencontre des espèces les plus fascinantes. Profitez du romantisme qui se dégage de cette île célèbre.
          </p>
          <img src="../../assets/image/dounayha/maldives2.jpg" alt="">
         </div>
-
-           <section class="DescriptionVillaPlage">
+            
+         
+           <section class="Container">
+               <section class="DescriptionVillaPlage">
               <h2>
                 VILLAS DE PLAGE
               </h2>
@@ -39,7 +42,7 @@
               <p>
                  Dans nos élégantes villas de plage s'ouvrant sur l'océan, faites l'expérience de la vie insulaire dans sa plus grande pureté.
               </p>
-           </section>
+              </section>
 
              <section class="DescriptionVillaPilotis">
              <h2>
@@ -50,7 +53,10 @@
              Profitez de la mer dans nos incroyables villas sur pilotis pour un séjour tropical inoubliable aux Maldives.
              </p>
              </section>
-           
+             
+            </section>
+       
+               
           
 
     </main>
