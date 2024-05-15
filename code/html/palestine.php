49ff5e314123591d811e5d06935e22f20ef0a815
AslanUnravel <aslanhadef9@gmail.com>
2024-05-15 16:58:05 +0200

    gg

---
+++ code/html/palestine.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Palestine</title>
+    <link rel="stylesheet" href="../css/palestine.css">
+</head>
+<body> 
+    <menu>
+        <h1>Présentation du japon</h1>
+        <p>Le Japon est un pays situé dans l'Est de l'Asie, composé d'une grande île principale et d'une multitude d'autres îles de différentes tailles </p>
+        <p>Le mont Fuji est une montagne du centre du Japon qui se trouve sur la côte sud de l'île de Honshū, au sud-ouest de l'agglomération de Tokyo </p> 
+      <h1>
+        <h2> Avec 3 776 mètres d'altitude, il est le point culminant du Japon. Situé dans une région où se rejoignent les plaques tectoniques pacifique, eurasienne et philippine, la montagne est un stratovolcan toujours considéré comme actif, sa dernière éruption certaine s'étant produite fin 1707, bien que le risque éruptif soit actuellement considéré comme faible.</h2>
+        <h2>À son sommet a été construit un observatoire météorologique et malgré les conditions climatiques rigoureuses, la montagne est une destination extrêmement populaire en particulier pour les Japonais, qu'ils soient shintoïstes ou bouddhistes, en raison de sa forme caractéristique et du symbolisme</h2>
+        <img src =" ../../assets/image/aslan/Mont-Fiji-JapanRoads.jpg " alt="image japon">
+        <p class = "asset "> Que faut-il voir en voyage au Japon ?</p>
+        
+        <section class="shabibi">
+          <div class="habibi">
+          <h3> Monument japonais : le palais impérial de Tokyo </h3>
+          <img src="../../assets/aslan/tokyo-imperial-palace.jpg" alt=" ">
+          </div>
+          
+        <div>
+        <P class="palais">Le palais de Tokyo est un incontournable lorsque l’on voyage au Japon, <br> et qu'on voit en cours de japonais en ligne. Son emplacement était autrefois la place de l'ancien château d'Edo, résidence officielle des shoguns Tokugawa jusqu'en 1868, date de la révolution Meiji. </P>
+
+        </div>
+        </section>
+
+     </menu>
+       
+    
+    s
+</body>
+
+ 
+
+</html>
