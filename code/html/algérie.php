63ac9289f1aa333ae767ef5a368b7d7a713ff6e9
bailhamza <h25667298@gmail.com>
2024-05-15 16:28:25 +0200

    mise à jour

--- code/html/algérie.php
+++ code/html/algérie.php
@@ -7,7 +7,7 @@
 </head>
 <body>
   <main>
-    <h1 class="algt">l'Algerie</h1>
+    <h1 class="algt">l'Algerie الجزائر</h1>
     <section class="simg">
     <img class="imgdrp" src="../../assets/image/hamza/drapeaualg.jpg" alt="">
     <img class="imgcarte" src="../../assets/image/hamza/catrealg.png" alt="">
@@ -24,14 +24,31 @@
 <section class="carpar">
    <section class="scaroussel1">
         <div class="caroussel1">
-            <img class="imgcaroussel1" src="../../assets/image/hamza/algc.jpg" alt="">
+          <div>
+            <p class="dcrousssel1">Monument des martyrs</p>
+           <img class="imgcaroussel1" src="../../assets/image/hamza/algc.jpg" alt="">
+          </div>
             <img class="imgcaroussel1" src="../../assets/image/hamza/alga.jpg" alt="">
+           
+           <div>
+           <p class="dcrousssel1">Jardin d'essai du Hamma</p>
             <img class="imgcaroussel1" src="../../assets/image/hamza/algd.jpg" alt="">
-            <img class="imgcaroussel1" src="../../assets/image/hamza/alge.jpg" alt="">
+           </div>
+           <div>
+           <p>Basilique Notre-Dame d'Afrique</p>
             <img class="imgcaroussel1" src="../../assets/image/hamza/algf.jpg" alt="">
+           </div>
+            <img class="imgcaroussel1" src="../../assets/image/hamza/alge.jpg" alt="">
+           <div>
+           <p class="dcrousssel1">Djamaâ El-Djazaïr</p>
             <img class="imgcaroussel1" src="../../assets/image/hamza/algg.jpg" alt="">
-            <img class="imgcaroussel1" src="../../assets/image/hamza/algh.jpg" alt="">
+           </div>
+           <div>
+           <p class="dcrousssel1">Parc national du Tassili N'Ajjer</p> 
             <img class="imgcaroussel1" src="../../assets/image/hamza/algb.jpg" alt="">
+           </div>
+            <img class="imgcaroussel1" src="../../assets/image/hamza/algh.jpg" alt="">
+           
         </div>
     </section>
   <section class="par">
@@ -47,11 +64,63 @@
 </section>
 <h2 id="capitale">Capitale</h2>
 <section class="salger">
+  <div>
   <p class="palger">Alger (en arabe : الجزائر العاصمة, Al-Jazāʾir El ʿĀṣimanote 1,2, en berbère : ⵍⴷⵣⴰⵢⵔ ⵜⴰⵎⴰⵏⴰⵖⵜ, Ldzayer tamanaɣt ou Lezzayer tamanaɣt3), surnommée El Bahdja (« la joyeuse »), El Mahrussa (« la bien-gardée ») ou El Beida (« la blanche »), est la capitale de l'Algérie et en est la ville la plus peuplée.</p>
   <p class="palger">Située au bord de la mer Méditerranée, la ville d'Alger est en fait constituée de plusieurs communes de la wilaya d'Alger dont elle donne son nom en tant que chef-lieu mais n'a ni personnalité juridique, ni structure d'administration en propre. L'unité urbaine d'Alger comptait 2 481 788 habitants selon l'Office national des statistiques algérien d'après le dernier recensement de 20084.</p>
   <p class="palger">Fondée au ive siècle av. J.-C. comme comptoir par les phéniciens, sous le nom d'Icosium, elle est occupée par les Romains, les Vandales, les Byzantins et les Arabes, puis au début du Moyen Âge par la tribu berbère des Beni-Mezghana, avant d'être récupérée en 1204 par la tribu arabe des Thaâliba qui règneront sur la région d'Alger jusqu'en 15168.</p>
+  </div>
+  <img class="imgalger" src="../../assets/image/hamza/a.jpg" alt="">
+  
+ 
 </section>
-   
+ <h2 class="tastim" id="lieux">Lieux à visiter</h2> 
+<section class="stimgad">
+  <div>
+  <p class="ptimgad">Timgad ou Thamugadi (colonie Marciana Traiana Thamugadi en latin), surnommée la <br> « Pompéi de l'Afrique du Nord »1 est une cité antique située sur le territoire de la commune <br> homonyme de Timgad, dans la wilaya de Batna dans la <br> région des Aurès, au Nord-Est de l'Algérie.</p>
+  <p class="ptimgad">Elle fut fondée par l'empereur romain Trajan en 100 et dotée du statut de colonie. <br> Il s'agit de la dernière « déduction de colonie » en Afrique romaine, <br> c'est-à-dire d'une colonie essentiellement peuplée de citoyens romains <br> (souvent d'ailleurs d'anciens soldats). Bâtie avec ses temples, <br> ses thermes, son forum et son théâtre, la ville, initialement d'une superficie <br> de 12 hectares, finit par en occuper plus de 90. Au vu <br> de son excellent état de conservation et du fait qu'on la considérait comme <br> typique d'une ville romaine, Timgad a été classée au patrimoine mondial de l'humanité par l'UNESCO en 1982. <br> La conservation du site soulève cependant un certain nombre de problèmes.</p>
+  </div>
+  <section class="scaroussel2">
+    <div class="divimgtim">
+    <img class="imgcaroussel2" src="../../assets/image/hamza/tima.jpg" alt="">
+    <img class="imgcaroussel2" src="../../assets/image/hamza/timb.jpg" alt="">
+    <img class="imgcaroussel2" src="../../assets/image/hamza/timc.jpg" alt="">
+    <img class="imgcaroussel2" src="../../assets/image/hamza/timd.jpg" alt="">
+    <img class="imgcaroussel2" src="../../assets/image/hamza/time.jpg" alt="">
+    <img class="imgcaroussel2" src="../../assets/image/hamza/timf.jpg" alt="">
+    <img class="imgcaroussel2" src="../../assets/image/hamza/timg.jpg" alt="">
+    <img class="imgcaroussel2" src="../../assets/image/hamza/timh.jpg" alt="">
+    <img class="imgcaroussel2" src="../../assets/image/hamza/timi.jpg" alt="">
+    <img class="imgcaroussel2" src="../../assets/image/hamza/timj.jpg" alt="">
+
+    </div>
+ 
+  </section>
+ 
+</section>
+<h2>Parc culturel du Tassili</h2>
+
+<section>
+  <div>
+  <p class="ptimgad">Le parc culturel du Tassili (anciennement parc national du Tassili jusqu'à 20111), est un parc national algérien, <br> situé près de la ville de Djanet, dans la wilaya de Illizi, dans le Sud de l'Algérie2.</p>
+  <p class="ptimgad">Couvrant une superficie de 138 000 km2, il est le premier parc d'Algérie par la taille. <br> Le parc englobe une grande partie du massif du Tassili n'Ajjer, l'erg Admer et la Tadrart Rouge3.</p>
+  <p class="ptimgad">L'occupation humaine de la région remonte à plus de 10 000 ans, lorsque des hommes néolithiques <br> l'arpentaient pour chasser et pratiquer la cueillette.</p>
+  <p class="ptimgad">Il est classé depuis 1982 au patrimoine mondial4 et réserve de biosphère depuis 1986 par l'UNESCO5,6. <br> Il est, par le nombre de ses gravures rupestres, le premier site à l'échelle mondiale.</p>
+  <p class="ptimgad">Le parc abrite également la vallée d'Iherir, classée site Ramsar depuis le 2 février 2017.</p>
+  </div>
+ <section>
+  <div>
+    <img src="../../assets/image/hamza/tasa.jpg" alt="">
+    <img src="../../assets/image/hamza/tasb.jpg" alt="">
+    <img src="../../assets/image/hamza/tasc.jpg" alt="">
+    <img src="../../assets/image/hamza/tasd.jpg" alt="">
+    <img src="../../assets/image/hamza/tase.jpg" alt="">
+    <img src="../../assets/image/hamza/tasef.jpg" alt="">
+    <img src="../../assets/image/hamza/taseg.jpg" alt="">
+  </div>
+ </section>
+</section>
+
+
 </main>
   </body>
 </html>
