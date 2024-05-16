2400199bd25c93466095ae6e7b9468deaa2d586f
Ibrahimpalisse <ibrahim.vignes@laplateforme.io>
2024-05-17 00:48:55 +0200

    commit3
    90%

--- code/html/suisse.php
+++ code/html/suisse.php
@@ -16,10 +16,18 @@
 </header>
 
     <main>
-      <section class="container1">
+     <!-- <section class="container1">
           <h1>LA SUISSE</h1>
           <img class="imagemobile1" src="../../assets/image/ibrahim/suiss1.png" alt="">
-     </section>
+     </section>-->
+     <section class="video-background">
+    <video autoplay loop muted playsinline>
+        <source src="../../assets/image/ibrahim/ESSAI2.mp4" type="video/mp4">
+    </video>
+    <div class="content">
+        <h1>LA SUISSE</h1>
+    </div>
+</section>
       
             
      <section class="container2">
@@ -37,7 +45,7 @@
          </h2>
     
        <img class="drapeau" src="../../assets/image/ibrahim/suisse3.jpg" alt="">
-         <p class="texte1">
+         <p>
             La Suisse, un pays d’Europe centrale,<br/>
             est réputée pour ses magnifiques<br/> paysages,
             sa stabilité politique<br/> et son économie prospère.<br/>
@@ -59,7 +67,7 @@
                <img src="../../assets/image/ibrahim/suisse5.jpg" alt="">
              </div>
               
-              <p class="texte1">
+              <p>
                   La Suisse est habituellement divisée </br>
                   en trois grandes zones géographiques.</br>
                   Du nord au sud, ainsi que par superficie</br>
@@ -82,58 +90,46 @@
     
       
       <div class="container5">
-          <a
-             href="https://www.myswitzerland.com/fr-fr/destinations/geneve/">
-             <img src="../../assets/image/ibrahim/suisse6.jpeg" alt="Image">
           
-      <div class="overlay">
-          
+             <img class="imggnève" src="../../assets/image/ibrahim/suisse6.jpeg" alt="Image">
+      <div class="overlay1">
         <h2>
             Genève
         </h2>
-          
-            <p>
-               Genève est la ville la plus internationale de Suisse.
-               C’est ici que se trouve le siège européen de l’ONU,
-               c’est ici que la Croix-Rouge dirige ses opérations.
-               La ville située au bord du lac Léman est également
-               le berceau de l’horlogerie et le lieu d’origine de 
-               nombreux chocolatiers. Elle offre de nombreuses
-               curiosités telles que le Jet d’eau, l’Horloge
-               fleurie, la vieille ville avec la cathédrale
-               St-Pierre ou la place du Bourg-de-Four.
-            </p>
+        <p>
+            Genève est la ville la plus internationale de Suisse.
+            C’est ici que se trouve le siège européen de l’ONU,
+            c’est ici que la Croix-Rouge dirige ses opérations.
+            La ville située au bord du lac Léman est également
+            le berceau de l’horlogerie et le lieu d’origine de 
+            nombreux chocolatiers. Elle offre de nombreuses
+            curiosités telles que le Jet d’eau, l’Horloge
+            fleurie, la vieille ville avec la cathédrale
+            St-Pierre ou la place du Bourg-de-Four.
+        </p>
       </div>
-    </a>
   </div>
 
-  <div class="container5">
-        <a 
-           href="https://www.myswitzerland.com/fr-fr/destinations/lausanne/">
-           <img src="../../assets/image/ibrahim/suiss7.jpeg" alt="Image">
-      <div class="overlay">
-
-        <h2>
-           Lausanne
-        </h2>
-          
+  <div class="container6">
+           <img class="imgLausanne" src="../../assets/image/ibrahim/suiss7.jpeg" alt="Image">
+        <div class="overlay2">
+            <h2>
+                Lausanne
+            </h2>
             <p>
-              Lausanne, la deuxième plus grande ville au bord
-              du lac Léman, a le dynamisme d’une cité d’affaires
-              tout en jouissant de la situation idéale d'une station
-              de villégiature. La capitale du canton de Vaud est en
-              outre une ville universitaire animée et une ville de
-              congrès. Dans la capitale olympique, sport et culture
-              s’inscrivent en lettres d’or.
-            </p>
-      </div>
-    </a>
+                Lausanne, la deuxième plus grande ville au bord
+                du lac Léman, a le dynamisme d’une cité d’affaires
+                tout en jouissant de la situation idéale d'une station
+                de villégiature. La capitale du canton de Vaud est en
+                outre une ville universitaire animée et une ville de
+                congrès. Dans la capitale olympique, sport et culture
+                s’inscrivent en lettres d’or.
+              </p>  
+        </div>
   </div>
-  <div class="container5">
-         <a
-             href="https://www.bern.com/fr/detail/vieille-ville-de-berne">
-             <img src="../../assets/image/ibrahim/suisse8.png" alt="Image">
-      <div class="overlay">
+  <div class="container7">
+             <img class="imgberne" src="../../assets/image/ibrahim/suisse8.png" alt="Image">
+      <div class="overlay3">
         <h2>
           Berne
         </h2>
@@ -149,37 +145,29 @@
       </div>
     </a>
   </div>
-  <div class="container5">
-          <a href="https://www.myswitzerland.com/fr-fr/destinations/zermatt/">
-             <img src="../../assets/image/ibrahim/suiss9.jpg" alt="Image">
-        <div class="overlay">
+  <div class="container8">
+             <img class="imgZermatt" src="../../assets/image/ibrahim/suiss9.jpg" alt="Image">
+        <div class="overlay4">
         <h2>
              Zermatt
         </h2>
-          
-        
-             
-             <p>
-                Zermatt se trouve au pied du Cervin. Son développement
-                est étroitement lié à ce sommet, qui est sans doute l'une
-                des montagnes les plus célèbres au monde. Ce village de
-                vacances sans voiture a conservé son caractère authentique
-                et offre d'innombrables possibilités d'excursions.  
-             </p>
-             
-      </div>
-    </a>
+        <p>
+          Zermatt se trouve au pied du Cervin. Son développement
+          est étroitement lié à ce sommet, qui est sans doute l'une
+          des montagnes les plus célèbres au monde. Ce village de
+          vacances sans voiture a conservé son caractère authentique
+          et offre d'innombrables possibilités d'excursions.  
+        </p>  
+    </div>
   </div>
 
   </section>
   </section>
       
   <section class="container55552">
-      <div class="container5">
-          <a
-             href="https://www.myswitzerland.com/fr-ch/destinations/montreux-riviera/">
-             <img src="../../assets/image/ibrahim/suiss11.png" alt="Image">
-      <div class="overlay">
+      <div class="container9">
+             <img  class="imgMontreux" src="../../assets/image/ibrahim/suiss11.png" alt="Image">
+      <div class="overlay5">
           
         <h2>
            Montreux
@@ -192,14 +180,11 @@
             de lettres et de voyageurs en quête de beauté, de calme et… d’inspiration.
             </p>
      </div>
-    </a>
   </div>
 
-  <div class="container5">
-        <a 
-           href="https://www.myswitzerland.com/fr-ch/destinations/les-grisons/">
-           <img src="../../assets/image/ibrahim/suisse14.png" alt="Image">
-      <div class="overlay">
+<div class="container10">
+           <img class="imggrisons" src="../../assets/image/ibrahim/suisse14.png" alt="Image">
+      <div class="overlay6">
         <h2>
            Les Grisons
         </h2>
@@ -213,36 +198,25 @@
      </div>
     </a> 
   </div>
-  <div class="container5">
-         <a
-             href="https://www.myswitzerland.com/fr-ch/destinations/grindelwald/">
-             <img src="../../assets/image/ibrahim/suisse13.png" alt="Image">
-        
-      <div class="overlay">
-
-        <h2>
-           Grindelwald
-        </h2>
-          
-            <p>
-               Grindelwald, le village de l’Eiger dans l’Oberland bernois,
-               est niché dans une adorable vallée verdoyante, entourée d’imposants sommets,
-               tels que la paroi nord de l’Eiger et le Wetterhorn. Ce panorama grandiose ainsi
-               que les innombrables points de vue et activités proposés font de Grindelwald
-               l’une des destinations touristiques les plus prisées et cosmopolites de Suisse
-               et le plus grand domaine skiable de la région de la Jungfrau.
-
-
-            </p>
-      </div>
-    </a> 
-  </div>
-  <div class="container5">
-          <a
-              href="https://www.myswitzerland.com/fr-ch/destinations/appenzell/">
-             <img src="../../assets/image/ibrahim/suisse15.png" alt="Image">
-          
-      <div class="overlay">
+    <div class="container11">
+             <img class="imggrindelwald" src="../../assets/image/ibrahim/suisse13.png" alt="Image">
+       <div class="overlay7">
+            <h2>
+              Grindelwald
+            </h2>
+              <p>
+                Grindelwald, le village de l’Eiger dans l’Oberland bernois,
+                est niché dans une adorable vallée verdoyante, entourée d’imposants sommets,
+                tels que la paroi nord de l’Eiger et le Wetterhorn. Ce panorama grandiose ainsi
+                que les innombrables points de vue et activités proposés font de Grindelwald
+                l’une des destinations touristiques les plus prisées et cosmopolites de Suisse
+                et le plus grand domaine skiable de la région de la Jungfrau.
+              </p>
+        </div>
+     </div>
+      <div class="container12">
+             <img class="imgappenzell" src="../../assets/image/ibrahim/suisse15.png" alt="Image">
+      <div class="overlay8">
 
         <h2>
             Appenzell
@@ -262,7 +236,91 @@
   </div>
 
   </section>
-  
+  <section class="backgroundbouffe">
+  <section class="texte1" >
+      <h2>
+        culinaires suisse
+      </h2>
+      <P>
+        La Suisse est réputée pour sa cuisine diversifiée et ses</br>
+        spécialités régionales. Voici quelques plats et spécialités</br>
+        culinaires suisses populaires :
+      </P>
+  </section>
+  <section class="container13">
+     <section>
+          <img class="bouffe1" src="../../assets/image/ibrahim/bouffe1.png" alt="">
+         <div class="texte2"> 
+            <h3>
+              Fondue suisse
+            </h3>
+            
+            <p>
+                Un plat emblématique suisse où du fromage fondu est servi dans un caquelon. Traditionnellement, on y trempe du pain dans le fromage fondu.
+            </p>
+       </div> 
+     </section>
+     <section>
+          <img class="bouffe2" src="../../assets/image/ibrahim/bouffe2.jpg" alt="">
+          <div class="texte3">
+              <h3>
+                   Raclette
+              </h3>
+              <p>
+                  Du fromage à pâte molle est fondu et raclé sur des pommes de terre, accompagné de cornichons, d'oignons et de charcuterie.
+              </p>
+          </div>  
+     </section>
+     <section>
+          <img class="bouffe2" src="../../assets/image/ibrahim/bouffe3.jpg" alt="">
+          <div class="texte4">
+              <h3>
+                  Rösti
+              </h3>
+                <p>
+                  Un plat à base de pommes de terre râpées et frites, souvent servi comme accompagnement pour les plats principaux.
+                </p>
+          </div>
+                
+     </section>
+  </section>
+  <section class="container14">
+      <section>
+              <img class="bouffe2" src="../../assets/image/ibrahim/bouffe4.jpg" alt="">
+                 <div class="texte5">
+                    <h3>
+                      Bircher Muesli
+                    </h3>
+                    <p>
+                    Un mélange de flocons d'avoine, de fruits frais, de noix et de yaourt, souvent consommé au petit-déjeuner.
+                    </p>
+                </div>  
+        </section>
+        <section class="soucontainer1">
+          <img class="bouffe3" src="../../assets/image/ibrahim/bouffe5.jpg" alt="">
+            <div class="texte6">
+              <h3>
+                    Zürcher Geschnetzeltes
+              </h3>
+              <p>
+                 Des morceaux de veau émincés cuits dans une sauce à la crème, servis avec des rösti.
+              </p>
+           </div> 
+     </section>
+     <section class="soucontainer2">
+          <img class="bouffe4" src="../../assets/image/ibrahim/bouffe6.jpg" alt="">
+          <div class="texte7">
+              <h3>
+                  Tartiflette
+              </h3>
+              <p>
+                 Un plat suisse originaire de la région du Valais, composé de pommes de terre, de lardons, d'oignons et de fromage à raclette.
+              </p>
+           </div>    
+     </section>
+  </section>
+  </section>      
+
     </main>
     
 <footer>
