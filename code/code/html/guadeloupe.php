46d9ac004c529d894144c4a707867e02eca38028
rubin-bourgeois <rubin.bourgeois@laplateforme.io>
2024-05-16 16:11:22 +0200

    pas fini

---
+++ code/code/html/guadeloupe.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/guadeloupe.css?=<?php time(); ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Guadeloupe</title>
+</head>
+
+<body>
+
+    <body>
+        <main>
+            <section class="firstsec">
+                <div class="firstdiv">
+                    <img src="../../assets/rubin-plage.jpg" alt="image_principal">
+                    <h1>La Guadeloupe et ses paysages</h1><br>
+                    <p>
+                        La Guadeloupe (en créole guadeloupéen : Gwadloup) est un archipel des Caraïbes constitué de sept
+                        îles, formant une région et un département d'outre-mer français. La partie principale (Karukera en
+                        langues amérindiennes2) en forme de papillon est composée de deux îles : la Grande-Terre à l'est et
+                        la Basse-Terre à l'ouest, séparées par un bras de mer, appelé « la Rivière Salée ». Le chef-lieu est
+                        Basse-Terre et la capitale économique est Pointe-à-Pitre.Administrativement, la Guadeloupe est une
+                        région ultrapériphérique de l'Union européenne, une région mono-départementale ainsi qu'un
+                        département de l'Outre-mer français, au sein de l'ensemble communément dénommé Antilles françaises ;
+                        son code départemental officiel est le « 971 ». Elle est membre de l’Organisation des États de la
+                        Caraïbe orientale (OECS).
+                        La Guadeloupe, bordée par la mer des Caraïbes à l'ouest et par l'océan Atlantique à l’est, est
+                        située à 575 km au nord des côtes du Venezuela en Amérique du Sud, à 205 km à l'est de l'Île d'Aves,
+                        (ou ile aux Oiseaux), à 739 km à l'est-sud-est de la République dominicaine, à 2 176 km à
+                        l'est-sud-est de Miami (États-Unis) et à 6 732 km de Paris.
+                    </p>
+                </div>
+            </section>
+            <section class="thirdsec">
+                <div class="thirdiv">
+                    <h2>Divers et variées</h2><br>
+                    <p>
+                        La Guadeloupe (en créole guadeloupéen : Gwadloup1) est un archipel des Caraïbes constitué de sept
+                        îles, formant une région et un département d'outre-mer français. La partie principale (Karukera en
+                        langues amérindiennes2) en forme de papillon est composée de deux îles : la Grande-Terre à l'est et
+                        la Basse-Terre à l'ouest, séparées par un bras de mer, appelé « la Rivière Salée ». Le chef-lieu est
+                        Basse-Terre et la capitale économique est Pointe-à-Pitre.Administrativement, la Guadeloupe est une
+                        région ultrapériphérique de l'Union européenne, une région mono-départementale ainsi qu'un
+                        département de l'Outre-mer français, au sein de l'ensemble communément dénommé Antilles françaises ;
+                        son code départemental officiel est le « 971 ». Elle est membre de l’Organisation des États de la
+                        Caraïbe orientale (OECS).
+                        La Guadeloupe, bordée par la mer des Caraïbes à l'ouest et par l'océan Atlantique à l’est, est
+                        située à 575 km au nord des côtes du Venezuela en Amérique du Sud, à 205 km à l'est de l'Île d'Aves,
+                        (ou ile aux Oiseaux), à 739 km à l'est-sud-est de la République dominicaine, à 2 176 km à
+                        l'est-sud-est de Miami (États-Unis) et à 6 732 km de Paris.
+                    </p>
+                    <img src="../../assets/rubin2.jpg" alt="image_principal">
+                </div>
+            </section>
+            <video id="background-video" autoplay loop muted>
+
+                <source src="../../assets/plage-seychelles-fond-d-ecran-animated-wallpaper.mp4" type="video/mp4">
+
+            </video>
+            <section class="escsec">
+                <div class="secondiv">
+                    <img src="../../assets/rubin-plage.jpg" alt="image_principal">
+                    <p>
+                        La Guadeloupe (en créole guadeloupéen : Gwadloup1) est un archipel des Caraïbes constitué de sept
+                        îles, formant une région et un département d'outre-mer français. La partie principale (Karukera en
+                        langues amérindiennes2) en forme de papillon est composée de deux îles : la Grande-Terre à l'est et
+                        la Basse-Terre à l'ouest, séparées par un bras de mer, appelé « la Rivière Salée». Le chef-lieu est
+                        Basse-Terre et la capitale économique est Pointe-à-Pitre.Administrativement, la Guadeloupe est une
+                        région ultrapériphérique de l'Union européenne, une région mono-départementale ainsi qu'un
+                        département de l'Outre-mer français, au sein de l'ensemble communément dénommé Antilles françaises ;
+                        son code départemental officiel est le « 971 ». Elle est membre de l’Organisation des États de la
+                        Caraïbe orientale (OECS).
+                        
+                    </p>
+                </div>
+                </section>
+                <section class="seche">
+                    <div class="content">
+                        <h2>Les repas Locaux savoureux</h2>
+                    </div>
+                </section>
+                <div class="beurre">
+                <h2>Repas local</h2>
+                <p>Sur cette île papillon des Petites Antilles, la baignade est toujours de saison. 
+                    Côté terre, une nature luxuriante entoure les villes et les villages, où se maintiennent culture et traditions créoles. 
+                    Un séjour en Guadeloupe peut combiner détente et sensations fortes puisque l’archipel regorge de jolies plages mais aussi de spots de plongée et de surf reconnus.
+                    Vos papilles vont être ravies pendant vos vacances guadeloupéennes : de l’apéro au dessert et jusqu’au goûter, 
+                    que vous soyez amateurs de fruits de mer ou de viande,
+                    bec sucré ou plutôt salé, prenez note, voici notre sélection des spécialités en Guadeloupe qu’il faudra tester au moins une fois pendant votre séjour ! 
+                </p>
+                </div>
+                <div class="carousel">
+                <!-- <h2>Description en image avec le caroussel ci-dessous</h2> -->
+                </div>
+                <div id="boite">
+                    <div id="carouselExample" class="carousel slide">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="../../assets/frubin1.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../../assets/frubin4.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../../assets/frubin3.jpg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+            </section>
+
+        </main>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+
+</html>
