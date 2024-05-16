6d64b42f194cdbd8500195ec4d36c4a883b18443
Yanis Bennadij <yanis.bennadji@laplateforme.io>
2024-05-16 16:03:16 +0200

    changement de plan ff

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
-    <link rel="stylesheet" href="./code/css/voyages.css">
+    <link rel="stylesheet" href="./code/css/voyages.css" <?= time(); ?>>
 </head>
 <body>
 
