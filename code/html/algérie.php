ac0b357f0b675e2329efc54ce8c9760691374806
bailhamza <h25667298@gmail.com>
2024-05-17 15:57:11 +0200

    Travail terminé

--- code/html/algérie.php
+++ code/html/algérie.php
@@ -21,36 +21,21 @@
           <a class="menu" href="#lieux"><h4 >Lieux à visiter</h4></a>
         </menu>
         </section>
-<section class="carpar">
-   <section class="scaroussel1">
+        <section class="carpar">
+
+        <section class="scaroussel1">
         <div class="caroussel1">
-          <div>
-            <p class="dcrousssel1">Monument des martyrs</p>
            <img class="imgcaroussel1" src="../../assets/image/hamza/algc.jpg" alt="">
-          </div>
             <img class="imgcaroussel1" src="../../assets/image/hamza/alga.jpg" alt="">
-           
-           <div>
-           <p class="dcrousssel1">Jardin d'essai du Hamma</p>
             <img class="imgcaroussel1" src="../../assets/image/hamza/algd.jpg" alt="">
-           </div>
-           <div>
-           <p>Basilique Notre-Dame d'Afrique</p>
             <img class="imgcaroussel1" src="../../assets/image/hamza/algf.jpg" alt="">
-           </div>
             <img class="imgcaroussel1" src="../../assets/image/hamza/alge.jpg" alt="">
-           <div>
-           <p class="dcrousssel1">Djamaâ El-Djazaïr</p>
             <img class="imgcaroussel1" src="../../assets/image/hamza/algg.jpg" alt="">
-           </div>
-           <div>
-           <p class="dcrousssel1">Parc national du Tassili N'Ajjer</p> 
             <img class="imgcaroussel1" src="../../assets/image/hamza/algb.jpg" alt="">
-           </div>
             <img class="imgcaroussel1" src="../../assets/image/hamza/algh.jpg" alt="">
-           
-        </div>
-    </section>
+          </div>
+             </section>
+         
   <section class="par">
     <p>L'Algérie , arabe : الجزائر (al-Jazā'ir)Note 2,Note 3 ; et arabe <br> algérien : دزاير (Dzayer) ou الجزاير (El Djazaïr) ; en tamazight ⴷⵣⴰⵢⵔ (Dzayer)) <br> est un pays d’Afrique du Nord faisant partie du Maghreb. Depuis 1962, <br> elle est nommée en forme longue République algérienne démocratique et populaire, <br> abrégée en (RADP) (en arabe : الجمهورية الجزائرية الديمقراطية الشعبية ; en tamazight <br>: ⵜⴰⴳⴷⵓⴷⴰ ⵜⴰⵎⴳⴷⴰⵢⵜ ⵜⴰⵖⵔⴼⴰⵏⵜ ⵜⴰⴷⵣⴰⵢⵔⵉⵜ (Tagduda tamegdayt taɣerfant tazzayrit))10. Sa capitale est <br>Alger, la ville la plus peuplée du pays, dans le Nord, sur la côte méditerranéenne.</p>
     <p>Au 1er janvier 2021, la population algérienne résidente avait atteint 44,6 millions <br> d’habitants11. Avec une superficie de 2 381 741 km2, c'est le <br> dixième plus grand pays au monde et le plus grand pays d'Afrique, <br> du monde arabe et du bassin méditerranéen. Elle partage plus de 6 343 km de frontières <br> terrestres, avec la Tunisie au nord-est, la Libye à l'est, le Niger <br> au sud-est, le Mali au sud-ouest, la Mauritanie et <br> le Sahara occidental à l'ouest, et enfin le Maroc au nord-ouest12.</p>
@@ -62,24 +47,28 @@
 <p class="pgeo">L'Algérie est un pays constitué d'une multitude de reliefs. Le nord est sillonné d'ouest en est <br> par une double barrière montagneuse (Atlas tellien et saharien) avec des chaînes telles que <br> le Dahra, l'Ouarsenis, le Hodna, les  chaines de Kabylie (le Djurdjura, les Babors et <br> les Bibans) et l'Aurès. Le sol est couvert de nombreuses forêts au centre, <br> vastes plaines à l'est et le Sahara qui représente à lui <br> seul 84 % du territoire. La superficie du pays est la plus grande d'Afrique, avec 2 381 741 km2 .</p>
 <p class="pgeo">Avec une superficie de 2 381 741 km2, c'est le dixième plus grand pays au monde et le plus grand pays d'Afrique du monde arabe et du bassin méditerranéen. Elle partage plus de 6 343 km de frontières terrestres, avec la Tunisie au nord-est, la Libye à l'est, le Niger au sud-est, le Mali au sud-ouest, la Mauritanie et le Sahara occidental à l'ouest, et enfin le Maroc au nord-ouest12.</p>
 </section>
+<section class="timalg">
 <h2 id="capitale">Capitale</h2>
 <h2 class="parc">Alger</h2>
+</section>
+
 <section class="salger">
-  <div>
-  <p class="palger">Alger (en arabe : الجزائر العاصمة, Al-Jazāʾir El ʿĀṣimanote 1,2, en berbère : ⵍⴷⵣⴰⵢⵔ ⵜⴰⵎⴰⵏⴰⵖⵜ, Ldzayer tamanaɣt ou Lezzayer tamanaɣt3), surnommée El Bahdja (« la joyeuse »), El Mahrussa (« la bien-gardée ») ou El Beida (« la blanche »), est la capitale de l'Algérie et en est la ville la plus peuplée.</p>
-  <p class="palger">Située au bord de la mer Méditerranée, la ville d'Alger est en fait constituée de plusieurs communes de la wilaya d'Alger dont elle donne son nom en tant que chef-lieu mais n'a ni personnalité juridique, ni structure d'administration en propre. L'unité urbaine d'Alger comptait 2 481 788 habitants selon l'Office national des statistiques algérien d'après le dernier recensement de 20084.</p>
-  <p class="palger">Fondée au ive siècle av. J.-C. comme comptoir par les phéniciens, sous le nom d'Icosium, elle est occupée par les Romains, les Vandales, les Byzantins et les Arabes, puis au début du Moyen Âge par la tribu berbère des Beni-Mezghana, avant d'être récupérée en 1204 par la tribu arabe des Thaâliba qui règneront sur la région d'Alger jusqu'en 15168.</p>
+  <div class="dalger">
+  <p class="palger">Alger (en arabe : الجزائر العاصمة, Al-Jazāʾir El ʿĀṣimanote 1,2, en berbère  <br>: ⵍⴷⵣⴰⵢⵔ ⵜⴰⵎⴰⵏⴰⵖⵜ, Ldzayer tamanaɣt ou Lezzayer tamanaɣt3), <br> surnommée El Bahdja (« la joyeuse »), El Mahrussa <br> (« la bien-gardée ») ou El Beida (« la blanche »), est la capitale de <br> l'Algérie et en est la ville la plus peuplée.</p>
+  <p class="palger">Située au bord de la mer Méditerranée, la ville d'Alger est en fait <br> constituée de plusieurs communes de la wilaya d'Alger dont elle donne <br> son nom en tant que chef-lieu . L'unité urbaine d'Alger comptait <br> 2 481 788 habitants selon l'Office national des statistiques algérien  d'après <br>  le dernier recensement de 2008.</p>
   </div>
   <img class="imgalger" src="../../assets/image/hamza/a.jpg" alt="">
-  
- 
 </section>
+  
+ <section class="timalg">
  <h2 class="tastim" id="lieux">Lieux à visiter</h2>
  <h2 class="parc">Timgad</h2> 
-<section class="stimgad">
-  <div>
+ </section>
+
+<section class="salger">
+  <div class="dalger">
   <p class="ptimgad">Timgad ou Thamugadi (colonie Marciana Traiana Thamugadi en latin), surnommée la <br> « Pompéi de l'Afrique du Nord »1 est une cité antique située sur le territoire de la commune <br> homonyme de Timgad, dans la wilaya de Batna dans la <br> région des Aurès, au Nord-Est de l'Algérie.</p>
-  <p class="ptimgad">Elle fut fondée par l'empereur romain Trajan en 100 et dotée du statut de colonie. <br> Il s'agit de la dernière « déduction de colonie » en Afrique romaine, <br> c'est-à-dire d'une colonie essentiellement peuplée de citoyens romains <br> (souvent d'ailleurs d'anciens soldats). Bâtie avec ses temples, <br> ses thermes, son forum et son théâtre, la ville, initialement d'une superficie <br> de 12 hectares, finit par en occuper plus de 90. Au vu <br> de son excellent état de conservation et du fait qu'on la considérait comme <br> typique d'une ville romaine, Timgad a été classée au patrimoine mondial de l'humanité par l'UNESCO en 1982. <br> La conservation du site soulève cependant un certain nombre de problèmes.</p>
+  <p class="ptimgad">Elle fut fondée par l'empereur romain Trajan en 100 et dotée du statut de colonie. <br> Il s'agit de la dernière « déduction de colonie » en Afrique romaine, <br> c'est-à-dire d'une colonie essentiellement peuplée de citoyens romains <br> (souvent d'ailleurs d'anciens soldats). Bâtie avec ses temples, <br> ses thermes, son forum et son théâtre, la ville, initialement d'une superficie <br> de 12 hectares, finit par en occuper plus de 90. Au vu <br> de son excellent état de conservation et du fait qu'on la considérait comme <br> typique d'une ville romaine, Timgad a été classée au patrimoine mondial de l'humanité <br> par l'UNESCO en 1982. <br> La conservation du site soulève cependant un certain nombre de problèmes.</p>
   </div>
   <section class="scaroussel2">
     <div class="divimgtim">
@@ -101,8 +90,8 @@
 </section>
 <h2 class="parc">Parc culturel du Tassili</h2>
 
-<section>
-  <div>
+<section class="dalger">
+  <div class="">
   <p class="ptimgad">Le parc culturel du Tassili (anciennement parc national du Tassili jusqu'à 2011), est un parc national algérien, <br> situé près de la ville de Djanet, dans la wilaya de Illizi, dans le Sud de l'Algérie2.</p>
   <p class="ptimgad">Couvrant une superficie de 138 000 km2, il est le premier parc d'Algérie par la taille. <br> Le parc englobe une grande partie du massif du Tassili n'Ajjer, l'erg Admer et la Tadrart Rouge3.</p>
   <p class="ptimgad">L'occupation humaine de la région remonte à plus de 10 000 ans, lorsque des hommes néolithiques <br> l'arpentaient pour chasser et pratiquer la cueillette.</p>
@@ -110,14 +99,14 @@
   <p class="ptimgad">Le parc abrite également la vallée d'Iherir, classée site Ramsar depuis le 2 février 2017.</p>
   </div>
  <section>
-  <div>
-    <img class="" src="../../assets/image/hamza/tasa.jpg" alt="">
-    <img src="../../assets/image/hamza/tasb.jpg" alt="">
-    <img src="../../assets/image/hamza/tasc.jpg" alt="">
-    <img src="../../assets/image/hamza/tasd.jpg" alt="">
-    <img src="../../assets/image/hamza/tase.jpg" alt="">
-    <img src="../../assets/image/hamza/tasf.jpg" alt="">
-    <img src="../../assets/image/hamza/tasg.jpg" alt="">
+  <div class="">
+    <img class="imgtas" src="../../assets/image/hamza/tasa.jpg" alt="">
+    <img class="imgtas" src="../../assets/image/hamza/tasb.jpg" alt="">
+    <img class="imgtas" src="../../assets/image/hamza/tasc.jpg" alt="">
+    <img class="imgtas" src="../../assets/image/hamza/tasd.jpg" alt="">
+    <img class="imgtas" src="../../assets/image/hamza/tase.jpg" alt="">
+    <img class="imgtas" src="../../assets/image/hamza/tasf.jpg" alt="">
+    <img class="imgtas" src="../../assets/image/hamza/tasg.jpg" alt="">
   </div>
  </section>
 </section>
