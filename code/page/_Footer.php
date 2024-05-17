5657030ca0b0ff2003d6d25c6ca04f405b51b20e
Yanis Bennadij <yanis.bennadji@laplateforme.io>
2024-05-17 11:22:45 +0200

    Enorme commit, index,header,footer,italie
    Plus gros commit avec changement global de toutes les pages, et du CSS. Ajouts d'énormément de fichier. Un des derniers plus importants

--- code/page/_Footer.php
+++ code/page/_Footer.php
@@ -5,39 +5,59 @@
         <p>Avec la participation de :</p>
         <ul>
             <li>
-                Malik
+                <a class="gitlink" href="https://github.com/Guiiizmo">Malik</a>
             </li>
             <li>
-                Ibrahim
-            </li>
-            <li>
-                Sabrina
-            </li>
-            <li>
-                Maïla
-            </li>
-            <li>
-                Hamza
+                <a class="gitlink" href="https://github.com/Ibrahimpalisse">Ibrahim</a>
             </li>
         </ul>
+        
         <ul>
             <li>
-                Adham
+                <a class="gitlink" href="https://github.com/hsabrina1">Sabrina</a>
             </li>
             <li>
-                Aslan
-            </li>
-            <li>
-                Yanis
-            </li>
-            <li>
-                Rubin
-            </li>
-            <li>
-                Dounayha
+                <a class="gitlink" href="https://github.com/Dounayha">Dounayha</a>
             </li>
         </ul>
+        
+        <ul>
+            <li>
+                <a class="gitlink" href="https://github.com/yanis-bennadji">Yanis</a>
+            </li>
+            <li>
+                <a class="gitlink" href="https://github.com/adham-merfoud">Adham</a>
+            </li>
+
+            
+        </ul>
+        
+        <ul>
+            <li>
+                <a class="gitlink" href="https://github.com/rubin-bourgeois">Rubin</a>
+            </li>
+            <li>
+                <a class="gitlink" href="https://github.com/bailhamza">Hamza</a>
+            </li>
+        </ul>
+
+        <ul>
+            <li>
+                <a class="gitlink" href="https://github.com/Maiilaa">Maïla</a>
+            </li>
+            <li>
+                <a class="gitlink" href="https://github.com/AslanUnravel">Aslan</a>
+            </li>
+        </ul> 
+
+        
+
+       
     </section>
+
+    <div class="end-footer">
+        <p>Projet dans le cadre de formation La Plateforme.</p>
+    </div>
 
 </footer>
 
